a29111485aab36925a17407331ba7dd8fc2b52eb
Christian Törnqvist <din@adress.com>
2024-10-14 20:00:18 +0200

    first commit, raging warrior added

---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Fantasyspel_lektion;
+using System.Collections.Generic;
+
+// Huvudprogrammet
+namespace fantasygame
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerHealth = 5000; // sätt spelarens starthälsa
+
+            // skapa en lista med fiender
+            List<Enemy> enemies = new List<Enemy>();
+            enemies.Add(new Mage("Saruman"));
+            //enemies.Add(new Assassin("Hitman"));
+            //enemies.Add(new Mage("Sauron"));
+            //enemies.Add(new Assassin("James Bond"));
+            enemies.Add(new Warrior("Badonka slayer"));
+            enemies.Add(new Warrior("Uruk-Hai"));
+
+            // spelloop - spelet körs så länge spelaren har hälsa kvar
+            while (playerHealth > 0)
+            {
+                // Skriv ut en meny
+                Console.WriteLine("========================================");
+                Console.WriteLine($"Du har {playerHealth} hälsa");
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("Det finns följande fiender:");
+                List<int> invisibleEnemyIndexes = new List<int>();
+                for (int i = 0; i < enemies.Count; i++)
+                {
+                    if (enemies[i].GetInfo() != null) // kontrollerar så att fienden är synlig
+                    {
+                        Console.WriteLine($"{(i + 1)}.  {enemies[i].GetInfo()}"); // skriv ut den synliga fienden
+                    }
+                    else // detta är en osynlig fiende (den returnerade "")
+                    {
+                        // Ingen utskrift här...
+                        invisibleEnemyIndexes.Add(i); // lägg till indexet i listan med osynliga fiender
+                    }
+                }
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("Välj vad du vill göra:");
+                Console.WriteLine("1. Attackera");
+                Console.WriteLine("2. Heala");
+                Console.WriteLine("3. Avsluta");
+                Console.Write("Ange alternativ 1-3:");
+                string input = Console.ReadLine();
+
+                Random random = new Random(); // används för att slumpa skada och häloregenerering
+                switch (input)
+                {
+                    case "1": // Attackera en viss fiende
+                        Console.Write("Vilken fiende vill du attackera:");
+                        // läs in vem spelaren vill attackera,
+                        // tag värde -1 för att få rätt index i listan
+                        int enemyIndex = int.Parse(Console.ReadLine()) - 1;
+                        // om spelaren valt en osynlig fiende, skriv ut felmeddelande och hoppa ur switchen
+                        if (invisibleEnemyIndexes.Contains(enemyIndex))
+                        {
+                            Console.WriteLine("Du kan inte attackera en osynlig fiende");
+                            break;
+                        }
+                        Enemy e = enemies[enemyIndex];
+                        // ge fienden skada:
+                        int damage = 200 + random.Next(0, 20);
+                        System.Console.WriteLine($"Du attackerar {e.Name} för {damage} skada");
+                        e.Defend(damage);
+                        break;
+
+                    case "2": // Heala sig själv
+                        Console.WriteLine("Du healar dig själv");
+                        playerHealth += 50 + random.Next(0, 50);
+                        break;
+
+                    case "3": // Avsluta spelet
+                        Console.WriteLine("Du avslutar spelet");
+                        return;
+
+                    default:
+                        Console.WriteLine("Felaktig input");
+                        break;
+                }
+
+                // ---------------------------------------------
+                // Gå igenom alla fiender, en efter en och:
+                //
+                // OM:      en fiende har 0 eller mindre hälsa, skriv att den är död
+                //          och ta bort den ur listan
+                // 
+                // ANNARS   låt fienden attackera spelaren 
+                // ---------------------------------------------
+                for (int i = 0; i < enemies.Count; i++)
+                {
+                    // om fienden har 0 eller mindre hälsa, döda den
+                    if (enemies[i].Health <= 0)
+                    {
+                        Console.WriteLine($"{enemies[i].Name} dog");
+                        // ta bort fienden från listan utifrån dess index
+                        enemies.RemoveAt(i);
+
+                        // i och med att denna fiende tas bort från listan så kommer 
+                        // alla andra fiender att flyttas ett steg uppåt i listan
+                        // Därför måste vi minska i med 1 för att inte hoppa över en fiende
+                        i--;
+                        // Vi hoppar över resten av loopen och går till nästa iteration.
+                        continue;
+                    }
+                    // om vi inte kör continue ovan, riskerar vi att hamna out of bounds
+                    // , på sista elementet
+
+                    // Fienden gör sin attack på spelaren:
+                    playerHealth -= enemies[i].Attack();
+                }
+
+                // ---------------------------------------------
+                // Kontrollera om spelaren har dött, isf avsluta mainloopen
+                if (playerHealth <= 0)
+                {
+                    Console.WriteLine("Du dog.");
+                    break;
+                }
+
+                else if (enemies.Count <= 0 && invisibleEnemyIndexes.Count <= 0)
+                {
+                    Console.WriteLine("YOU'RE THE MASTER OF MIDDLE EARTH CONGRATULATIONS MOTHER FUCKER");
+                }
+                // Vänta på att användaren ska trycka på en tangent och rensa skärmen
+                Console.ReadKey();
+                Console.Clear();
+            }
+            Console.WriteLine("Spelet är slut");
+        }
+    }
+}
---
+++ enemy.cs
@@ -0,0 +1,199 @@
+﻿// TODO: skapa fler attacker
+
+// Basklass för alla fiender
+using System;
+
+namespace Fantasyspel_lektion
+{
+
+
+    class Enemy
+    {
+        // Alla fiender ska ha detta gemensamt:
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int BaseDamage { get; set; }
+        public int Armor { get; set; }
+
+        // Metoder som alla fiender ska ha. Dessa anropas med hjälp av polymorfism:
+
+        // En metod för att ge info information om fienden
+        public virtual string GetInfo() // ?
+        {
+            return $"{Name} har {Health} hälsa";
+        }
+
+        // Fienden attackerar.
+        public virtual int Attack()
+        {
+            return 0;
+        }
+
+        // Fienden försvarar sig.
+        public virtual void Defend(int damage)
+        {
+            int totalDamage = damage - Armor;
+            System.Console.WriteLine($"{Name} tar {totalDamage} skada");
+            Health -= totalDamage;
+        }
+    }
+
+    class Warrior : Enemy
+    { // TODO: rage, varje gång han gör mer skada får han mer rage. Mortal strike = 50 baseDamage + random.Next
+      // + När han tar dmg så får han också rage
+      // Vad ska standardattacken vara?
+        public int Rage { get; set; }
+
+        public Warrior(string name)
+        {
+
+            Random random = new Random();
+            Name = name;
+            Health = 30 + random.Next(0, 80);
+            BaseDamage = 15;
+            Armor = 20;
+            Rage = 0;
+        }
+
+        public override int Attack()
+        {//             damage = BaseDamage + random.Next(0, 30);
+
+            int damage;
+            int mortalStrikeLimit;
+            Random random = new Random();
+            damage = BaseDamage + random.Next(0, 35);
+            if (Rage >= 50) // MORTAL STRIKE
+            { 
+                damage = random.Next(30, 70);
+                Console.WriteLine($"{Name} gets ragefueled and hits you with MORTAL STRIKE and damages you for {damage}");
+                Rage += random.Next(1, 15);
+                Rage -= 50;
+                Console.WriteLine("Rage är2: " + Rage);
+                return damage;
+
+            }
+            else
+            {
+                damage = BaseDamage + random.Next(0, 35);
+                Rage += random.Next(1, 31);
+                Console.WriteLine($"{Name} smacks you with his axe for {damage}");
+                Console.WriteLine("Rage är3: " + Rage);
+                return damage;
+            }
+        }
+        public override void Defend(int damage)
+        {
+            Random random = new Random();
+            int totalDamage = damage - Armor;
+            Rage += random.Next(0, 10);
+            System.Console.WriteLine($"{Name} tar {totalDamage} skada");
+            Health -= totalDamage;
+        }
+
+    }
+
+    // En klass för att hålla reda på en lönnmördare
+    // Denna kan vi sen skapa flera lönnmördare (instanser) av
+    // Denna ärver av basklassen Enemy
+    class Assassin : Enemy
+    {
+        // Förutom de egenskaper som finns i basklassen Enemy, har lönnmördaren möjligheten
+        // att vara osynlig
+        bool isVisible;
+
+        // Konstruktor
+        public Assassin(string name)
+        {
+            Random random = new Random();
+            Health = 20 + random.Next(0, 80);
+            BaseDamage = 20;
+            Armor = 15;
+            isVisible = false; // TODO: random på/av???
+            Name = name;
+        }
+
+        // Ger information om lönnmördaren.
+        public override string  GetInfo() /*?*/
+        {
+            if (isVisible)
+                return base.GetInfo();
+            else
+                return null;
+        }
+
+        // lönnmördaren attackerar olika beroende på om den är synlig eller inte:
+        public override int Attack()
+        {
+            int damage;
+            // if it is visible, it will attack
+            Random random = new Random();
+
+            if (isVisible)
+            {
+                damage = BaseDamage + random.Next(0, 30);
+                Console.WriteLine($"{Name} hugger dig med sin vassa kniv och gör {damage} skada");
+                isVisible = false;
+                return damage;
+            }
+            else
+            {
+                damage = BaseDamage + random.Next(0, 10);
+                Console.WriteLine($"någonstans ifrån kommer en pil som ger dig {damage} skada");
+                isVisible = true;
+                return damage;
+            }
+        }
+
+        // lönnmördaren försvarar sig men bara om den är synlig, annars tar den ingen skada 
+        public override void Defend(int damage)
+        {
+            if (isVisible)
+            {
+                base.Defend(damage);
+            }
+        }
+    }
+
+    // En klass för att hålla reda på en magiker.
+    // Denna kan vi sen skapa flera magiker (instanser) av
+    class Mage : Enemy
+    {
+        // Egenskaper
+        public int Mana { get; set; }
+
+        // konstruktor för att skapa magikern:
+        public Mage(string name)
+        {
+            BaseDamage = 10;
+            Random random = new Random();
+            Health = 20 + random.Next(0, 80);
+            Mana = 20 + random.Next(0, 80);
+            Armor = 5;
+            Name = name;
+        }
+
+        // Magikern attackerar
+        // return value - denna metod returnerar ett heltal som är skadan som magikern gör
+        public override int Attack()
+        {
+            int damage; // hur mycket skada ska magikern göra?
+
+            // om magikern har mer än 10 mana, kan den kasta en eldboll
+            // TODO: Lägg till fler attacker som vi slumpar emellan
+            if (Mana > 10)
+            {
+                Random random = new Random();
+                damage = BaseDamage + random.Next(0, 30);
+                Console.WriteLine($"{Name} kastar en eldboll som gör {damage} skada");
+                Mana -= 10;
+            }
+            else // magikern har inte tillräckligt med mana för att kasta en eldboll
+            {
+                Console.WriteLine($"{Name} har för lite mana för att kunna attackera");
+                Mana += 5;
+                damage = 0;
+            }
+            return damage;
+        }
+    }
+}
